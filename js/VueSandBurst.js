1505e4e32358ea45ed93f6aa2291a2a4859910da
wwwwxl <1306780059@qq.com>
2020-01-26 16:46:13 +0800

    add a content 51cto example about sand 渐变进度条和沙包变化

---
+++ js/VueSandBurst.js
@@ -0,0 +1,19 @@
+var app=new Vue({
+	el:"#vue-app",
+	data:{
+		health:100,
+		ended:false
+	},
+	methods:{
+		punch:function(){
+			this.health-=10;
+			if(this.health<10){
+				this.ended=true;
+			}
+		},
+		restart:function(){
+			this.health=100;
+			this.ended=false;
+		}
+	}
+})
